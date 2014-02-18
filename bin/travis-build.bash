fa03bc9af2d9826cb606ede9a99cd85808053830
Sean Hammond <seanhammond@lavabit.com>
2014-02-18 20:41:52 +0100

    Fix the Travis build
    There still seems to be a problem with Solr refusing connections though,
    but the tests are passing without it.
    
    Squashed commit of the following:
    
    commit 4f933e7c1e74162fdd75b3ec72c8ac882f1eb253
    Author: Sean Hammond <seanhammond@lavabit.com>
    Date:   Tue Feb 18 20:29:53 2014 +0100
    
        Add debug echoing to Travis build
    
        Also remove some stuff about old postgres versions that we don't need
    
    commit 3f58b39d9da0832c90dcc7aaeaa81d880bfb832e
    Author: Sean Hammond <seanhammond@lavabit.com>
    Date:   Tue Feb 18 20:17:29 2014 +0100
    
        Just do one Travis build with Python 2.7 and Postgres 9.1
    
    commit 0eb21d4a99e4c64a0f77f36d0ec4efaa62776e19
    Author: Sean Hammond <seanhammond@lavabit.com>
    Date:   Tue Feb 18 20:05:09 2014 +0100
    
        Travis build run db init from in ckan dir
    
    commit ebb4183e800fbccd7aba5b288080435c56d80455
    Author: Sean Hammond <seanhammond@lavabit.com>
    Date:   Tue Feb 18 19:58:29 2014 +0100
    
        Fix a broken path in the Travis build
    
    commit 21d0677e1b6138611d013aa161af74b733924e34
    Author: Sean Hammond <seanhammond@lavabit.com>
    Date:   Tue Feb 18 19:50:00 2014 +0100
    
        Make travis-build.bash executable
    
    commit f853637a2de62ae4b76a43a0fe3ece36a1e069a9
    Author: Sean Hammond <seanhammond@lavabit.com>
    Date:   Tue Feb 18 19:42:56 2014 +0100
    
        Try moving test.ini into a subdir instead

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -1,48 +1,40 @@
 #!/bin/bash
 set -e
 
-# Drop Travis' postgres cluster if we're building using a different pg version.
-TRAVIS_PGVERSION='9.1'
-if [ $PGVERSION != $TRAVIS_PGVERSION ]
-then
-  sudo -u postgres pg_dropcluster --stop $TRAVIS_PGVERSION main
-  # Make psql use $PGVERSION
-  export PGCLUSTER=$PGVERSION/main
-fi
+echo "This is travis-build.bash..."
 
-# Force Postgres 8.4 to use port 5432.
-if [ $PGVERSION == '8.4' ]
-then
-  sudo sed -i -e 's/port = 5433/port = 5432/g' /etc/postgresql/8.4/main/postgresql.conf
-fi
-
-sudo service postgresql restart
-
-# Install the packages that CKAN requires.
+echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
 sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
 
-# Install CKAN and its Python dependencies.
-cd /home/travis/build/ckan
+echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
 git checkout b2
 python setup.py develop
-pip install -r requirements.txt --use-mirrors
-cd /home/travis/build/ckan/ckanext-b2
+pip install -r requirements.txt
+cd -
 
-# Create the PostgreSQL database and users.
+echo "Creating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
 
-# Configure Solr.
+echo "Configuring Solr..."
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-sudo cp ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
-# Initialise the database.
+echo "Initialising the database..."
+cd ckan
 paster db init -c test-core.ini
+cd -
 
-# Install ckanext-b2 and its requirements.
+echo "Installing ckanext-b2 and its requirements..."
 python setup.py develop
 pip install -r dev-requirements.txt
+
+echo "Moving test.ini into a subdir..."
+mkdir subdir
+mv test.ini subdir
+
+echo "travis-build.bash is done."
