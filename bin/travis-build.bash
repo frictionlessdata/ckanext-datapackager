1bb2ae4dece12294b7ccd5aa08afb1dd322105c7
Sean Hammond <seanhammond@lavabit.com>
2014-02-19 21:58:35 +0100

    Move solr setup from Travis build to run
    Wild guess at why solr isn't working on travis

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -20,11 +20,6 @@ echo "Creating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
 
-echo "Configuring Solr..."
-echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-sudo service jetty restart
-
 echo "Initialising the database..."
 cd ckan
 paster db init -c test-core.ini
