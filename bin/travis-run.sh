6d2ed76997302fd805d0dcce625e288b27e6ef7a
Sean Hammond <seanhammond@lavabit.com>
2014-02-19 22:30:02 +0100

    More travis-run tweaks

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-set -e
+#!/bin/sh -e
 
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
