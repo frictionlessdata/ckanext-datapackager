c4d987fb45418aa02bd6135b015ce6555ef19b87
Sean Hammond <seanhammond@lavabit.com>
2014-02-20 17:36:37 +0100

    Exclude some boilerplate from Coveralls report

--- ckanext/b2/__init__.py
+++ ckanext/b2/__init__.py
@@ -1,7 +1,7 @@
 # this is a namespace package
-try:
+try:  # pragma: no cover
     import pkg_resources
     pkg_resources.declare_namespace(__name__)
-except ImportError:
+except ImportError:  # pragma: no cover
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
