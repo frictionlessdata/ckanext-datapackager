frictionlessdata/ckanext-datapackager

commit 5b5f4e8d6aa5722a4019af1edfedd5e2ca7622df

[#39] Use "url" when "upload" is falsy, not only None.

ckanext/datapackager/logic/action/create.py
@@ -29,7 +29,7 @@ def package_create_from_datapackage(context, data_dict):
     '''
     url = data_dict.get('url')
     upload = data_dict.get('upload')
-    if not url and upload is None:
+    if not url and not upload:
         msg = {'url': ['you must define either a url or upload attribute']}
         raise toolkit.ValidationError(msg)
 
@@ -65,7 +65,7 @@ def package_create_from_datapackage(context, data_dict):
 
 def _load_and_validate_datapackage(url=None, upload=None):
     try:
-        if upload is not None:
+        if upload:
             dp = datapackage.DataPackage(upload.file)
         else:
             dp = datapackage.DataPackage(url)

commit 9dd74becabb6e96d597f5e556eb30caf21bb5b96

[#39] Fix bug where uploaded datapackage wasn't being used
This problem was introduced in 5b5f4e8d. There, I've changed the test if a file
was uploaded from testing if `upload is None` to testing if `upload` is truthy.
It turns out `cgi.FieldStorage` is falsy, so this test was failing, and we were
instead using the `url` attribute.

With this patch, I test that `upload.file` and `upload.file.read` exists.

ckanext/datapackager/logic/action/create.py
@@ -29,7 +29,7 @@ def package_create_from_datapackage(context, data_dict):
     '''
     url = data_dict.get('url')
     upload = data_dict.get('upload')
-    if not url and not upload:
+    if not url and not _upload_attribute_is_valid(upload):
         msg = {'url': ['you must define either a url or upload attribute']}
         raise toolkit.ValidationError(msg)
 
@@ -65,7 +65,7 @@ def package_create_from_datapackage(context, data_dict):
 
 def _load_and_validate_datapackage(url=None, upload=None):
     try:
-        if upload:
+        if _upload_attribute_is_valid(upload):
             dp = datapackage.DataPackage(upload.file)
         else:
             dp = datapackage.DataPackage(url)
@@ -139,6 +139,10 @@ def _create_and_upload_resource(context, resource, the_file):
     resource['url_type'] = 'upload'
     resource['upload'] = _UploadLocalFileStorage(the_file)
     toolkit.get_action('resource_create')(context, resource)
+
+
+def _upload_attribute_is_valid(upload):
+    return hasattr(upload, 'file') and hasattr(upload.file, 'read')
 
 
 class _UploadLocalFileStorage(cgi.FieldStorage):