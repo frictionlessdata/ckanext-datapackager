797f0666c80fc28eb036d325e5d504e41d797fa1
roll <eskarev@gmail.com>
2020-02-08 11:13:21 +0300

    Fixed travis build

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -16,8 +16,8 @@ then
 fi
 
 python setup.py develop
-pip install -r requirements.txt --allow-all-external
-pip install -r dev-requirements.txt --allow-all-external
+pip install -r requirements.txt
+pip install -r dev-requirements.txt
 cd -
 
 echo "Creating the PostgreSQL user and database..."
