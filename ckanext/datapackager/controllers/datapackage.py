24c99a0da5c89bd014ff0323bdee817dc4260e71
amercader <amercadero@gmail.com>
2021-07-22 16:57:35 +0200

    Split controller and blueprint common functions to separate module

--- ckanext/datapackager/controllers/datapackage.py
+++ ckanext/datapackager/controllers/datapackage.py
@@ -3,93 +3,37 @@ import json
 import ckan.model as model
 import ckan.plugins.toolkit as toolkit
 
-def _authorize_or_abort(context):
-        try:
-            toolkit.check_access('package_create', context)
-        except toolkit.NotAuthorized:
-            toolkit.abort(401, toolkit._('Unauthorized to create a dataset'))
+from ckanext.datapackager import utils
 
-def new(data=None, errors=None, error_summary=None):
-    context = {
-        'model': model,
-        'session': model.Session,
-        'user': toolkit.c.user,
-        'auth_user_obj': toolkit.c.userobj,
-    }
-    _authorize_or_abort(context)
 
-    errors = errors or {}
-    error_summary = error_summary or {}
-    default_data = {
-        'owner_org': toolkit.request.params.get('group'),
-    }
-    data = data or default_data
+class DataPackageController(toolkit.BaseController):
+    def new(self, data=None, errors=None, error_summary=None):
+        return utils.new(data, errors, error_summary)
 
-    return toolkit.render(
-        'datapackage/import_datapackage.html',
-        extra_vars={
-            'data': data,
-            'errors': errors,
-            'error_summary': error_summary,
+    def import_datapackage(self):
+        return utils.import_datapackage()
+
+    def export_datapackage(self, package_id):
+        '''Return the given dataset as a Data Package JSON file.
+
+        '''
+        context = {
+            'model': model,
+            'session': model.Session,
+            'user': toolkit.c.user,
         }
-    )
+        r = toolkit.response
+        r.content_disposition = 'attachment; filename=datapackage.json'.format(
+            package_id)
+        r.content_type = 'application/json'
 
-def import_datapackage():
-    context = {
-        'model': model,
-        'session': model.Session,
-        'user': toolkit.c.user,
-    }
-    _authorize_or_abort(context)
+        try:
+            datapackage_dict = toolkit.get_action(
+                'package_show_as_datapackage')(
+                context,
+                {'id': package_id}
+            )
+        except toolkit.ObjectNotFound:
+            toolkit.abort(404, 'Dataset not found')
 
-    try:
-        params = toolkit.request.params
-        dataset = toolkit.get_action('package_create_from_datapackage')(
-            context,
-            params,
-        )
-        toolkit.redirect_to(controller='package',
-                            action='read',
-                            id=dataset['name'])
-    except toolkit.ValidationError as e:
-        errors = e.error_dict
-        error_summary = e.error_summary
-        return new(data=params,
-                        errors=errors,
-                        error_summary=error_summary)
-
-def export_datapackage(package_id):
-    '''Return the given dataset as a Data Package JSON file.
-
-    '''
-    context = {
-        'model': model,
-        'session': model.Session,
-        'user': toolkit.c.user,
-    }
-    r = toolkit.response
-    r.content_disposition = 'attachment; filename=datapackage.json'.format(
-        package_id)
-    r.content_type = 'application/json'
-
-    try:
-        datapackage_dict = toolkit.get_action(
-            'package_show_as_datapackage')(
-            context,
-            {'id': package_id}
-        )
-    except toolkit.ObjectNotFound:
-        toolkit.abort(404, 'Dataset not found')
-
-    return json.dumps(datapackage_dict, indent=2)
-
-if not toolkit.check_ckan_version(u'2.9'):
-    class DataPackageController(toolkit.BaseController):
-        def new(self, data=None, errors=None, error_summary=None):
-            new(data, errors, error_summary)
-        def import_datapackage(self):
-            import_datapackage()
-        def export_datapackage(self, package_id):
-            export_datapackage(package_id)
-
-
+        return json.dumps(datapackage_dict, indent=2)
--- ckanext/datapackager/plugin/flask_plugin.py
+++ ckanext/datapackager/plugin/flask_plugin.py
@@ -1,15 +1,10 @@
 import ckan.plugins as plugins
 
 from flask import Blueprint
-import ckanext.datapackager.controllers.datapackage as datapackage
+from ckanext.datapackager.blueprint import blueprint
 
 class MixinPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IBlueprint)
-    
+
     def get_blueprint(self):
-        blueprint = Blueprint('datapackager', __name__)
-        # As long as the URL for import_datapackage_view and import_datapackage are the same, reverse lookups from import_datapackage will work
-        blueprint.add_url_rule("/import_datapackage", view_func=datapackage.new, endpoint='import_datapackage', methods=['GET'])
-        blueprint.add_url_rule("/import_datapackage", view_func=datapackage.import_datapackage, endpoint='import_datapackage_post', methods=['POST'])
-        blueprint.add_url_rule("/dataset/<package_id>/datapackage.json", view_func=datapackage.export_datapackage, endpoint='export_datapackage', methods=['GET'])
-        return blueprint
+      return blueprint
