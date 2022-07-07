a19167935eb5734ccae040efbcd33c48840ba016
Edgar Zanella Alvarenga <e@vaz.io>
2022-07-07 14:59:00 +0200

    remove outdated nose.tools.assert_item_equals

--- ckanext/datapackager/tests/lib/test_converter.py
+++ ckanext/datapackager/tests/lib/test_converter.py
@@ -422,13 +422,13 @@ class TestDataPackageToDatasetDict(object):
             'location': {'country': 'China'},
         })
         result = converter.datapackage_to_dataset(self.datapackage)
-        nose.tools.assert_items_equal(result.get('extras'), [
+        assert result.get('extras') == [
             {'key': 'profile', 'value': u'data-package'},
             {'key': 'title_cn', 'value': u'國內生產總值'},
             {'key': 'years', 'value': '[2015, 2016]'},
             {'key': 'last_year', 'value': 2016},
             {'key': 'location', 'value': '{"country": "China"}'},
-        ])
+        ]
 
     def test_resource_name_is_used_if_theres_no_title(self):
         resource = {
