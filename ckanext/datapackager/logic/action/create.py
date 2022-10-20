c0372e0b5f20e3ec3c1a0f9702cd947839bb9f5b
Edgar Zanella Alvarenga <e@vaz.io>
2022-10-20 09:37:05 +0200

    support uploaded datapackage files in CKAN versions lower than 2.9

--- ckanext/datapackager/logic/action/create.py
+++ ckanext/datapackager/logic/action/create.py
@@ -88,7 +88,10 @@ def _load_and_validate_datapackage(url=None, upload=None):
     try:
 
         if _upload_attribute_is_valid(upload):
-            dp = datapackage.DataPackage(upload)
+            if toolkit.check_ckan_version(min_version="2.9"):
+                dp = datapackage.DataPackage(upload)
+            else:
+                dp = datapackage.DataPackage(upload.file)
         else:
 
             dp = datapackage.DataPackage(url)
@@ -186,7 +189,7 @@ def _create_and_upload_resource(context, resource, the_file):
 
 
 def _upload_attribute_is_valid(upload):
-    return hasattr(upload, 'read')
+    return hasattr(upload, 'read') or hasattr(upload, 'file') and hasattr(upload.file, 'read')
 
 
 class _UploadLocalFileStorage(FileStorage):
